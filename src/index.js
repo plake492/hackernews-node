ac6b469e0038f3ea9cbf7f7f724ce485e9a6e3d4
plake <plake492@gmail.com>
2019-10-28 11:21:58 -0400

    added prisma

--- src/index.js
+++ src/index.js
@@ -1,34 +1,20 @@
 const { GraphQLServer } = require("graphql-yoga");
-
-// 1
-// const typeDefs = `
-
-// `;
-
-let links = [
-  {
-    id: "link-0",
-    url: "www.howtographql.com",
-    description: "Fullstack tutorial for GraphQL"
-  }
-];
+const { prisma } = require("./generated/prisma-client");
 
 // 2
-let idCount = links.length;
 const resolvers = {
   Query: {
     info: () => `This is the API of Hackernews Clone`,
-    feed: () => links
+    feed: () => (root, args, context, info) => {
+      return context.prisma.links();
+    }
   },
   Mutation: {
-    post: (parent, args) => {
-      const link = {
-        id: `link-${idCount++}`,
-        description: args.description,
-        url: args.url
-      };
-      links.push(link);
-      return link;
+    post: (root, args, context) => {
+      return context.prisma.createLink({
+        url: args.url,
+        description: args.description
+      });
     }
   }
 };
@@ -36,6 +22,8 @@ const resolvers = {
 // 3
 const server = new GraphQLServer({
   typeDefs: "./src/schema.graphql",
-  resolvers
+  resolvers,
+  context: { prisma }
 });
+
 server.start(() => console.log(`Server is running on http://localhost:4000`));
